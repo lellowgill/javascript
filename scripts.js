3474d238dd3cf57e5e313d61bd9396878b2c07ba
deb <lellowgill@gmail.com>
2021-09-17 10:11:39 -0400

    exercises from Javascript

---
+++ scripts.js
@@ -0,0 +1,23 @@
+//THIS CREATES A NOTE FOR YOURSELF...
+
+console.log
+("This is the house that jack built");  
+console.log
+("Did you see the house that Jack built?")
+console.log("Deborium");
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof [1, 2, 3, 4]);
+console.log(new Date());
+
+//array it's like a list in python...
+function printName() {
+    console.log("Deb Wright");
+}
+printName();
+
+/**
+ * this is a multi line comment
+ * help i have wrote another note
+ * on the next line...
+ */
