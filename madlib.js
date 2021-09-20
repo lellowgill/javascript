2c251759f8fed4e31dc25534bec98666b6d32bd1
deb <lellowgill@gmail.com>
2021-09-20 13:46:17 -0400

    added madlib adjustments

---
+++ madlib.js
@@ -0,0 +1,21 @@
+//madlib exercise in practice
+//var myName = ('Deborium')
+//var subject = ('science')
+
+// var myFavorite = "";
+// var mySubject = "subject";
+// var wordIn = "in"
+// var mySchool = "school";
+// var wordIs = "is";
+// var myFavSub = "science.";
+
+function madLib(myName, subject) {
+    const message = `My name is ${myName}, my favorite subject is ${subject}.`
+
+    return message;
+
+}
+
+let student = madLib("Deboriums","science")
+
+console.log(student)
