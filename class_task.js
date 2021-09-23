4fc6f53409892fd088629028bda39724ec5f972f
deb <lellowgill@gmail.com>
2021-09-23 14:29:42 -0400

    mods added

--- class_task.js
+++ class_task.js
@@ -2,7 +2,7 @@
 //select the h1 tag and set "Toppings" as the text of the heading. 
 //select the list item 'burger'. Add a class of 'list-itme'
 //Create and append a new li element and append it to the list. 
-
+ 
 
 document.getElementById("Toppings").innerHTML = "Toppings";
 document.getElementById("burger").className = "list-item";
@@ -11,3 +11,4 @@ let onionsOne = document.createElement('li');
 document.getElementById('list').appendChild(onionsOne);
 
 onionsOne.innerHTML = 'onions';
+
