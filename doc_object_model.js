db1a053a1f41b7b145aa90992fa90ec0105586ef
deb <lellowgill@gmail.com>
2021-09-23 14:28:42 -0400

    added mods

--- doc_object_model.js
+++ doc_object_model.js
@@ -1,7 +1,35 @@
 //keyword D.O.M. document object Model
 //KEYWORD B.O.M. Browser Object Model
-//is a 
+//in Javascript you can do any data type
+//window.location will put up your current location 
 
-let userObjectModel = {
+
+/**let userObjectModel = {
     firstName: "Debbie"
+}*/
+//an object have (curly) braces key value pairs
+
+//console.log('BOM accessed through window object:')
+//console.log(window)
+
+/**let userObjectModel = {
+    firstName: 'Debbie',
+    age: 33
+} */
+//if you want to access the browser elements you use the keyword window
+//if you want to access the html  use the keyword document 
+console.log('DOM accessed through window object:')
+console.log(document)
+
+let userObject = {
+    sayHello: function() {
+        console.log('hello');
+    },
+    firstName: 'Debbie'
 }
+
+console.log('hello')
+
+let element = document.getElementsByTagName('h1')
+
+console.log(elements)
