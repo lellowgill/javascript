bad4b8a2a86cbd5b0896f4a0d5b8b3b96bbcb346
deb <lellowgill@gmail.com>
2021-09-23 14:32:47 -0400

    just the positives js

---
+++ just_the_positives.js
@@ -0,0 +1,22 @@
+function positiveNumbers (ar) {
+    const posArr = ar.filter(num => num > -1);
+    let positiveNumbers = [-5, 10, -3, 12, -9, 5, 90, 0, 1];
+    positiveNumbers.filter(posArr)
+    return posArr
+}
+
+console.log()
+
+
+
+/**let positiveNumbers = [-5, 10, -3, 12, -9, 5, 90, 0, 1];
+getPositives(numArr);
+
+// output: [10,12,5,90,0,1]
+
+/**function getPositives(ar){
+    const posArr = ar.filter(num => num > -1);
+    
+    return posArr;
+
+console.log()*/
