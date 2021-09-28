b2309b2f5189165e272faf14c4c08d9a39457cfb
deb <lellowgill@gmail.com>
2021-09-28 16:02:06 -0400

    added button

--- click_button.js
+++ click_button.js
@@ -2,6 +2,22 @@
 //the browser produces a signal when an evenrt occurs and provides a 'listener' 
 //to execute some code when the event occurs
 //an event is a reaction to the action taken by the user in the interface.
+//the alert() function is the event handler. 
+//To react to these different events we can assign an event handler. 
+
+/**Event	Triggered when
+ * 
+'click'	     the pointing device is clicked
+'resize'	 the size of the browser or window is changed
+'scroll'	 the window has been scroll
+'keydown'	 any key is pressed
+'keyup'	     any key is released
+'keypress'	 ANY key except Shift, Fn, CapsLock is in pressed position. (Fired continously.)
+'fullscreenchange'	 when the window has entered or exited full screen mode
+'dblclick'	the pointing device is double clicked
+'play'  	media starts playing
+'ended' 	media has finished playing
+# */
 
 document.querySelector('button')
 
