32c88110ca37114eaa409c54df90cbe16883f88c
deb <lellowgill@gmail.com>
2021-10-19 10:35:53 -0400

    js class in vscode

---
+++ classes.js
@@ -0,0 +1,15 @@
+/**
+ * [] 
+ * []
+ * []
+ *
+ */
+
+functionDog(name, color, bred) {
+    this.name = name;
+    this.color = color;
+    this.bred = bred;
+}
+
+let dog1 = new Dog("Clifford", "red", "big dog");
+let dog2 = new Dog("Spot", "black and white", "doberman");
