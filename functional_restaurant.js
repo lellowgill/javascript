e31bf70b16d26eeb9284ecee86a04caf7829ebd1
deb <lellowgill@gmail.com>
2021-10-20 11:02:56 -0400

    notes for the exercise

---
+++ functional_restaurant.js
@@ -0,0 +1,17 @@
+/**
+ * Create a restaurant class using an object contructor function and using the class keyword
+ * 
+ * This restaurant should be able to keep record of:
+ *      orders
+ *          status
+ *          items
+ *          total cost
+ *      a menu
+ *          items
+ *              item name
+ *              price
+ *      the ability to create an orders
+ *      this ability to add an item to an order
+ */
+
+
