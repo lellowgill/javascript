e414347dbe4b84b01949112d57c56c8421a61dd7
deb <lellowgill@gmail.com>
2021-11-08 11:19:48 -0500

    added two number sum function

---
+++ Two_Number_Sum.js
@@ -0,0 +1,15 @@
+/**
+ * Two Number Sum 
+ * 
+ * Sample Input
+ * array = [3, 5,-4, 8, 11, 1, -1, 6]
+ * targetSum = 10
+ * 
+ * Sample output
+ * [-1, 11] // the numbers could be in reverse order
+ * 
+ */
+
+function TwoNumberSum = [3, 5, -4, 8, -2]
+targetSum = [10]
+    
